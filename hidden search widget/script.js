11048e3cd2134c067e121a13c6db1e8205b7e7b5
yourusername <kunay69@hotmail.it>
2021-02-25 09:20:26 +0100

    hidden search widget basic functionality

--- hidden search widget/script.js
+++ hidden search widget/script.js
@@ -3,6 +3,6 @@ const btn = document.querySelector('.btn')
 const input = document.querySelector('.input')
 
 btn.addEventListener('click', () => {
-    search.classList.toggle('active')
-    input.focus()
+  search.classList.toggle('active')
+  input.focus()
 })
