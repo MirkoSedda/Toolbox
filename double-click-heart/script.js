894b6250d0d5db1a20f0a6b0a4623d749524c4fe
yourusername <kunay69@hotmail.it>
2021-03-04 16:06:22 +0100

    updated js

--- double-click-heart/script.js
+++ double-click-heart/script.js
@@ -4,39 +4,39 @@ const times = document.querySelector('#times')
 let clickTime = 0
 let timesClicked = 0
 
-loveMe.addEventListener('click', (e) => {
-    if(clickTime === 0) {
-        clickTime = new Date().getTime()
+loveMe.addEventListener('click', e => {
+  if (clickTime === 0) {
+    clickTime = new Date().getTime()
+  } else {
+    if (new Date().getTime() - clickTime < 800) {
+      createHeart(e)
+      clickTime = 0
     } else {
-        if((new Date().getTime() - clickTime) < 800) {
-            createHeart(e)
-            clickTime = 0
-        } else {
-            clickTime = new Date().getTime()
-        }
+      clickTime = new Date().getTime()
     }
+  }
 })
 
-const createHeart = (e) => {
-    const heart = document.createElement('i')
-    heart.classList.add('fas')
-    heart.classList.add('fa-heart')
+const createHeart = e => {
+  const heart = document.createElement('i')
+  heart.classList.add('fas')
+  heart.classList.add('fa-heart')
 
-    const x = e.clientX
-    const y = e.clientY
+  const x = e.clientX
+  const y = e.clientY
 
-    const leftOffset = e.target.offsetLeft
-    const topOffset = e.target.offsetTop
+  const leftOffset = e.target.offsetLeft
+  const topOffset = e.target.offsetTop
 
-    const xInside = x - leftOffset
-    const yInside = y - topOffset
+  const xInside = x - leftOffset
+  const yInside = y - topOffset
 
-    heart.style.top = `${yInside}px`
-    heart.style.left = `${xInside}px`
+  heart.style.left = `${xInside}px`
+  heart.style.top = `${yInside}px`
 
-    loveMe.appendChild(heart)
+  loveMe.appendChild(heart)
 
-    times.innerHTML = ++timesClicked
+  times.innerHTML = ++timesClicked
 
-    setTimeout(() => heart.remove(), 1000)
+  setTimeout(() => heart.remove(), 1000)
 }
